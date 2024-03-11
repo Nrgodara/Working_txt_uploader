b58a2d7b307940d77bce3ace325ad891b48056c4
Nrgodara <155721977+Nrgodara@users.noreply.github.com>
2024-03-11 22:39:07 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,5 @@
 api_id = "24825870"
 api_hash = "12ab9061b6260f9b91632947331ea0f6"
 bot_token = "7056746394:AAG6QPq2s4puTva4S7_i4t5_dHolfKiX5gI"
+premium_users = [6200744907, 987654321]  # List of premium user IDs
+
