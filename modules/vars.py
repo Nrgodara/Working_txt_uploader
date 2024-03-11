2069645c02bff8df97569a96762a92ae2485558c
Nrgodara <155721977+Nrgodara@users.noreply.github.com>
2024-03-11 23:10:37 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,5 +1,5 @@
 api_id = "24825870"
 api_hash = "12ab9061b6260f9b91632947331ea0f6"
-bot_token = "7056746394:AAG6QPq2s4puTva4S7_i4t5_dHolfKiX5gI"
-premium_users = [6200744907, 987654321]  # List of premium user IDs
-
+bot_token = "7006551345:AAGzOxmy4lUuy-6bc4gy108sx7s5Z4s-Jog"
+owner_user_id = YOUR_OWNER_USER_ID_HERE  # Replace YOUR_OWNER_USER_ID_HERE with the actual ID of the bot owner
+premium_users = [6200744907]  # This list can be used to store premium user IDs
