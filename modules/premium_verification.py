f7934756c89c05504cdc48ef62009975b3ab1339
Nrgodara <155721977+Nrgodara@users.noreply.github.com>
2024-03-12 07:02:59 +0530

    Update premium_verification.py

--- modules/premium_verification.py
+++ modules/premium_verification.py
@@ -1,14 +1,11 @@
-#premium.py
+# premium_verification.py
 
 from pyrogram import Client, filters
-from pyrogram.types import Message
-from pyrogram.errors import FloodWait
-from pyrogram.errors.exceptions.bad_request_400 import StickerEmojiInvalid
-from pyrogram.types.messages_and_media import message
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ForceReply
-from pyrogram.types import CallbackQuery
+from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
+from vars import premium_users
 
-
+# Define qr_code_filename or import it from another module if necessary
+qr_code_filename = "qr_code.jpg"
 
 async def verify_premium_user(user_id):
     return user_id in premium_users
